a1d0245470010b1036b6efdd67b046212755825d
songyun.ding <songyun.ding@gumichina.com>
2015-11-25 18:45:38 +0800

    init repository

---
+++ Assets/App/Editor/ActionEditPanel.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ActionEditPanel : EditorWindow {
+
+	private const string ACTION_EDITOR_SCENE = "Assets/App/Scenes/ActionEditor.unity";
+
+	private string m_ActionPath = string.Empty;
+	private string m_CharaPath = string.Empty;
+	private string m_WeaponPath = string.Empty;
+
+	private GameObject m_CharaSource;
+
+	private GameObject m_WeaponSource;
+
+	[MenuItem ("Tools/Action Edit Panel")]
+	public static void OpenEditPanel () {
+		EditorWindow.GetWindow <ActionEditPanel> (false, "Action Edit");
+	}
+
+	private void OnGUI () {
+		{ // Column 1
+			EditorGUILayout.BeginHorizontal ();
+			// Load Sceme
+			if (GUILayout.Button ("Load Scene")) {
+				EditorApplication.OpenScene (ACTION_EDITOR_SCENE);
+			}
+
+			// Load Action
+			if (GUILayout.Button ("Load Action")) {
+				m_ActionPath = EditorUtility.OpenFilePanel ("Open Action...", Application.dataPath + "/App/ExternalResources/Action", "asset"); 
+			}
+
+			// Save Action
+			if (GUILayout.Button ("Save Action")) {
+			
+			}
+
+			// Clear Action
+			if (GUILayout.Button ("Clear Action")) {
+			}
+			EditorGUILayout.EndHorizontal ();
+		}
+
+		{ // Column 2
+			EditorGUILayout.BeginVertical ("Box");
+			// Setup Scene
+			GUILayout.Label ("Setup Scene");
+
+
+			EditorGUILayout.BeginHorizontal ();
+			// Set Character
+			if (GUILayout.Button ("Load Chara", GUILayout.Width (120))) {
+				m_CharaPath = EditorUtility.OpenFilePanel ("Load Character...", Application.dataPath + "/App/ExternalResources/Character/Prefabs", "prefab"); 
+				CreateCharacter ();
+			}
+				
+			GUILayout.TextField (string.IsNullOrEmpty (m_CharaPath) ? "" : m_CharaPath.Substring (m_CharaPath.IndexOf ("Character")));
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal ();
+			// Set Character
+			if (GUILayout.Button ("Load Weapon", GUILayout.Width (120))) {
+				m_WeaponPath = EditorUtility.OpenFilePanel ("Load Weapon...", Application.dataPath + "/App/ExternalResources/Weapon/Prefabs", "prefab"); 
+				CreateWeapon ();
+			}
+
+			GUILayout.TextField (string.IsNullOrEmpty (m_WeaponPath) ? "" : m_WeaponPath.Substring (m_WeaponPath.IndexOf ("Weapon")));
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal ();
+			GUILayout.FlexibleSpace ();
+			// Clean Scene
+			if (GUILayout.Button ("Clean Scene", GUILayout.Width (120))) {
+				CleanScene ();
+			}
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.EndVertical ();
+		}
+	}
+
+	private void SetupScene () {
+		
+
+
+	}
+
+	private void CreateCharacter () {
+		CleanWeapon ();
+		CleanCharacter ();
+
+		m_CharaSource = AssetDatabase.LoadAssetAtPath (m_CharaPath.Substring (m_CharaPath.IndexOf ("Assets")), typeof(GameObject)) as GameObject;
+		m_CharaSource = GameObject.Instantiate (m_CharaSource) as GameObject;
+
+		Transform charaTran = m_CharaSource.transform;
+		charaTran.parent = ActionEditorManager.I.anchorChara;
+		charaTran.localPosition = Vector3.zero;
+		charaTran.localScale = Vector3.one;
+		charaTran.localRotation = Quaternion.identity;
+
+	}
+
+	private void CleanCharacter () {
+		if (m_CharaSource != null)
+			DestroyImmediate (m_CharaSource);
+
+		m_CharaSource = null;
+	}
+
+	private void CreateWeapon () {
+		if (m_CharaSource == null)
+			return;
+
+		CleanWeapon ();
+
+		m_WeaponSource = AssetDatabase.LoadAssetAtPath (m_WeaponPath.Substring (m_WeaponPath.IndexOf ("Assets")), typeof(GameObject)) as GameObject;
+		m_WeaponSource = GameObject.Instantiate (m_WeaponSource) as GameObject;
+
+		Transform wepTran = m_WeaponSource.transform;
+		wepTran.parent = SearchTransform (m_CharaSource.transform, "WepPos_R");
+		wepTran.localPosition = Vector3.zero;
+		wepTran.localScale = Vector3.one;
+		wepTran.localRotation = Quaternion.identity;
+	}
+
+	private void CleanWeapon () {
+		if (m_WeaponSource != null)
+			DestroyImmediate (m_WeaponSource);
+
+		m_WeaponSource = null;
+	}
+
+	private void CleanScene () {
+		CleanWeapon ();
+		CleanCharacter ();
+	}
+
+	public Transform SearchTransform( Transform fromTrans, string name )
+	{
+		Transform[] transforms = fromTrans.GetComponentsInChildren <Transform> (true);
+
+		Transform ret = null;
+
+		if (transforms.Length <= 0)
+			return ret;
+
+		foreach (var trans in transforms) {
+			if (trans.name.Equals (name)) {
+				ret = trans;
+				break;
+			}
+		}
+
+		return ret;
+	}
+}
---
+++ Assets/App/ExternalResources/Shaders/OutlineSilhouetteToon.shader
@@ -0,0 +1,126 @@
+﻿Shader "Custom/Outlined/Silhouetted Toon" {
+	Properties {
+		_Color (		"Main Color", Color) = (1,1,1,1)
+		_OutlineColor (	"Outline Color", Color) = (0,0,0,1)
+		_Outline (		"Outline width", Range (0.0, 0.03)) = .0005
+		_MainTex (		"Base (RGB)", 2D) = "white" { }
+		_RampTex (		"Ramp (RGB)", 2D) = "white" { }
+		_AddColor (		"Add Color", Color) = (0,0,0,1)
+	}
+	
+	CGINCLUDE
+	#include "UnityCG.cginc"
+	
+	half4		_Color;
+	sampler2D	_MainTex;
+	half4		_MainTex_ST;
+	sampler2D	_RampTex;
+	half4		_AddColor;
+	
+	struct appdata {
+		half4 vertex : POSITION;
+		half3 normal : NORMAL;
+		half2 texcoord : TEXCOORD;
+	};
+
+	struct outline_v2f {
+		half4 pos		: POSITION;
+		half4 color	: COLOR;
+		half4 texcoord	: TEXCOORD0;
+	};
+	struct base_v2f {
+		half4 pos		: POSITION;
+		half4 color	: COLOR;
+		half4 texcoord	: TEXCOORD0;
+	};
+
+	uniform half _Outline;
+	uniform half4 _OutlineColor;
+	
+	//global variable
+	half4 _LightDir = half4(-1,0.2,-0.5,0);
+	
+	outline_v2f outline_vert(appdata v) {
+		// just make a copy of incoming vertex data but scaled according to normal direction
+		outline_v2f o;
+		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+	
+		half3 norm   = mul ((half3x3)UNITY_MATRIX_IT_MV, v.normal);
+		half2 offset = TransformViewToProjection(norm.xy);
+	
+		o.pos.xy += offset * o.pos.z * _Outline;
+		o.color = _OutlineColor;
+		return o;
+	}
+	base_v2f base_vert(appdata v) {
+		// just make a copy of incoming vertex data but scaled according to normal direction
+		base_v2f o;
+		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+	
+		half4	mv_norm		= normalize( mul (UNITY_MATRIX_MV, half4( v.normal,0 ) ) );
+		half4	m_norm		= normalize( mul (_Object2World, half4( v.normal,0 ) ) );
+		half3	light_direction = normalize( _LightDir );
+		o.color.r = dot( light_direction, m_norm.xyz );
+		o.color.g = (m_norm.y + 1) * 0.5;
+		
+		o.texcoord.xy	= TRANSFORM_TEX( v.texcoord, _MainTex);
+		
+		return o;
+	}
+	ENDCG
+
+	SubShader {
+		Tags { "Queue" = "Geometry" }
+		// note that a vertex shader is specified here but its using the one above
+		Pass {
+			Name "OUTLINE"
+			Tags { "LightMode" = "Always" }
+			Cull Back
+			ZWrite Off
+			ZTest Always
+			ColorMask RGBA
+
+			// you can choose what kind of blending mode you want for the outline
+			Blend SrcAlpha OneMinusSrcAlpha // Normal
+			//Blend One One // Additive
+			//Blend One OneMinusDstColor // Soft Additive
+			//Blend DstColor Zero // Multiplicative
+			//Blend DstColor SrcColor // 2x Multiplicative
+
+			CGPROGRAM
+			#pragma vertex outline_vert
+			#pragma fragment frag
+			
+			half4 frag(outline_v2f i) : COLOR {
+				return half4( i.color.rgb, 1 );
+			}
+			ENDCG
+		}
+		Pass {
+			Name "BASE"
+			Tags { "LightMode" = "Always" }
+			Cull Back
+			ZWrite On
+			ZTest LEqual
+			Blend SrcAlpha OneMinusSrcAlpha
+			Material {
+				Diffuse [_Color]
+				Ambient [_Color]
+			}
+			Lighting Off
+			CGPROGRAM
+			#pragma vertex base_vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			half4 frag( base_v2f i ) :COLOR {
+				half4 albedo = tex2D( _MainTex, i.texcoord.xy );
+				half lambert = tex2D( _RampTex, half2( saturate( i.color.r ), 0) );
+				return half4( lerp( albedo*albedo, albedo, lambert ).rgb, albedo.a ) * _Color + _AddColor;
+			}
+			ENDCG
+		}
+	}
+	
+	Fallback "Diffuse"
+}
